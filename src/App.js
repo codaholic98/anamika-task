5a5f871e701bfc6c9854b8921ee0b0676a069781
ajay-chaubey <ajaychaubey95@gmail.com>
2021-11-28 23:13:34 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { TextField, Button } from '@mui/material';
+import "./App.css"
+import CircularProgress from '@mui/material/CircularProgress';
+import axios from 'axios';
 
-function App() {
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default function BasicGrid() {
+  let [quantity, setQuantity] = React.useState(0);
+  let [loading, setLoading] = React.useState(true)
+
+  const plusQuantity = () => {
+    setQuantity(quantity + 1)
+    updateValue()
+  }
+  const minusQuantity = () => {
+    setQuantity(quantity - 1)
+    updateValue()
+  }
+
+  React.useEffect(() => {
+    // axios.get(url, header).then(res => {
+    //   setQuantity()
+    // })
+  }, [])
+
+  const updateValue = () => {
+    // let data = {
+    //   'counter1': quantity
+    // }
+    // axios.put(link, data)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ flexGrow: 1 }} className="App">
+      <Grid container spacing={2}>
+        <Grid item lg={12} style={{textAlign:"center"}}>
+          {loading ? <> <CircularProgress /> {"saving counter value"} </>: "" }
+        </Grid>
+        <Grid item lg={12}>
+          <Button variant="outlined" onClick={minusQuantity}>{'<'}</Button>
+          <TextField id="outlined-basic" variant="outlined" className="input-cus" onChange={(event) => {
+            if (event.target.value > 1000) {
+              return
+            } else {
+              if (event.target.value != "")
+                setQuantity(parseInt(event.target.value))
+              else
+                setQuantity(0)
+            }
+            updateValue()
+          }}
+            value={quantity}
+            style={{height:3}}
+          />
+          <Button variant="outlined" onClick={plusQuantity}>{'>'}</Button>
+
+        </Grid>
+        <Grid item lg={12}>
+          <h5>Counter value: {quantity}</h5>
+        </Grid>
+
+      </Grid>
+    </Box>
   );
 }
-
-export default App;
